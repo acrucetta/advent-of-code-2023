344f7698edd0670af49822ed287615950bcb2b4d
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-11-17 17:47:36 -0600

    setting up 2022 files

---
+++ aoc/bin/main.ml
@@ -0,0 +1,40 @@
+module P = Command.Param
+open Command.Let_syntax
+
+let year_arg =
+  Command.Arg_type.create (fun year_str ->
+    match Int.of_string_opt year_str with
+    | Some year when year = 2022 || year = 2023 -> year
+    | _ -> failwith "Year must be [2022, 2023]")
+;;
+
+let day_arg =
+  Command.Arg_type.create (fun day_str ->
+    match Int.of_string_opt day_str with
+    | Some day when 1 <= day && day <= 25 -> day
+    | _ -> failwith "Day must be between 1 - 25")
+;;
+
+let part_arg =
+  Command.Arg_type.create (fun part_str ->
+    match Int.of_string_opt part_str with
+    | Some part when part = 1 || part = 2 -> part
+    | _ -> failwith "Part must be 1 or 2")
+;;
+
+let command_param =
+  let%map year =
+    P.flag
+      "year"
+      (P.optional_with_default 2022 year_arg)
+      ~aliases:[ "y" ]
+      ~doc:"int year to run (default: 2022)"
+  and day =
+    P.flag "day" (P.required day_arg) ~aliases:[ "d" ] ~doc:"int day to run (1 - 25)"
+  and part =
+    P.flag "part" (P.required part_arg) ~aliases:[ "p" ] ~doc:"int part to run (1 or 2)"
+  in
+  fun _ -> Runner.run year day part
+;;
+
+let () = Command_unix.run (Command.basic command_param ~summary:"Run aoc solution")
---
+++ aoc/lib/runner/runner.ml
@@ -0,0 +1,14 @@
+let run2022 day part input =
+  match day, part with
+  | 1, 1 -> Printf.printf "%i\n" (Year2022.Day01.part1 input)
+  | 1, 2 -> Printf.printf "%i\n" (Year2022.Day01.part2 input)
+  | day, part -> failwith (Printf.sprintf "Unknown day %i and part %i" day part)
+;;
+
+let run year day part =
+  let path = Printf.sprintf "data/%i/day%02i.txt" year day in
+  let input = In_channel.read_all path in
+  match year with
+  | 2022 -> run2022 day part input
+  | year -> failwith (Printf.sprintf "Unknown year: %i" year)
+;;
