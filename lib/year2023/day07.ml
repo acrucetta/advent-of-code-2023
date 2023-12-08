971bea3ce2829471b6a9d4e77715c9254286cfc3
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-12-08 10:11:14 -0600

    day 7: refactored code to use char maps

--- lib/year2023/day07.ml
+++ lib/year2023/day07.ml
@@ -39,28 +39,24 @@ open List
 
 let sp on = Str.split (Str.regexp on)
 
-type cards = 
-  | A 
-  | K 
-  | Q
-  | J
-  | T
-  | N of int
+type card =
+  | Ace | King | Queen | Jack | Ten | Nine | Eight | Seven | Six | Five | Four | Three | Two
+  [@@deriving compare]
 
 let card_to_int = function
-  | A -> 14
-  | K -> 13
-  | Q -> 12
-  | J -> 11
-  | T -> 10
-  | N n -> n
-
-type hand = cards list
-
-type round = {
-  hand: hand;
-  bid: int;
-}
+  | Ace -> 14
+  | King -> 13
+  | Queen -> 12
+  | Jack -> 11
+  | Ten -> 10
+  | Nine -> 9
+  | Eight -> 8
+  | Seven -> 7
+  | Six -> 6
+  | Five -> 5
+  | Four -> 4
+  | Three -> 3
+  | Two -> 2
 
 type hand_type = 
   | FiveOfAKind 
@@ -70,7 +66,51 @@ type hand_type =
   | TwoPair
   | OnePair
   | HighCard
+  [@@deriving compare, enumerate, equal]
 
+type round = {
+  hand: card list;
+  bid: int;
+  hand_type: hand_type;
+}
+
+(* [hand_to_type hand] returns the type [hand_type] of the hand.
+
+  To do this, we need to parse each distinct string in the hand and count how many
+  cards of the same type we have. Then we match them to the hand_type. If there are
+  no matches, we have a high card.
+
+  To do this, we will create a frequency map with each string.
+ *)
+
+let hand_to_type hand =
+  let freq_map = String.fold hand ~init:(Map.empty (module Char)) ~f:(fun acc c ->
+    match Map.find acc c with
+    | Some v -> Map.set acc ~key:c ~data:(v + 1)
+    | None -> Map.set acc ~key:c ~data:1
+  ) in
+  let freq_map = Map.data freq_map |> List.sort ~compare:Int.compare in
+  match freq_map with
+  | [1; 1; 1; 1; 1] -> HighCard
+  | [2; 3] -> FullHouse
+  | [1; 1; 1; 2] -> OnePair
+  | [1; 2; 2] -> TwoPair
+  | [1; 1; 3] -> ThreeOfAKind
+  | [1; 4] -> FourOfAKind
+  | [5] -> FiveOfAKind
+  | _ -> failwith "Invalid hand"
+
+let parse_rounds input =
+  let lines = sp "\n" input in
+  let rounds = map ~f:(fun line ->
+    let line = sp " " line in
+    let hand = nth_exn line 0 in
+    let bid = nth_exn line 1 |> Int.of_string in
+    let hand_type = hand_to_type hand in
+    { hand; bid; hand_type }
+  ) lines in
+  rounds
+;;
 
 let part1 input =
   -1
